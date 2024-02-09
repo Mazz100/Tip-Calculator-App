e80bb4e9d3aa2e075aa89ea60d3f3fd4a74c603a
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2024-02-09 18:25:56 +0200

    Desktop Layout update

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
       <footer class="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-        Coded by <a href="#">Your Name Here</a>.
+        Coded by <a href="https://github.com/Mazz100">Mazen Hassan</a>.
       </footer>
     </>
   )
